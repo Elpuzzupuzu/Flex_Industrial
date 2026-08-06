b71da5fa590fd2ce567f6a3820fb435ed65d7b1f
Elpuzzupuzu <tu_email_de_github>
2026-08-06 11:25:12 -0600

    ajustes en la seccion de productos pagina principal y mock creados

--- web/src/pages/projetcs/Projects.tsx
+++ web/src/pages/projetcs/Projects.tsx
@@ -1,10 +1,9 @@
 import FeaturedProjectsCarousel from "@/components/projects/FeaturedProjectsCarousel/FeaturedProjectsCarousel";
-import ProjectsHero from "@/components/projects/ProjectsHero/ProjectsHero";
+// import ProjectsHero from "@/components/projects/ProjectsHero/ProjectsHero";
 
 function Projects() {
   return (
     <main className="min-h-screen overflow-hidden bg-white">
-      {/* <ProjectsHero /> */}
 
       <FeaturedProjectsCarousel />
     </main>
